afdf22cee139997aa316584ceec80a2cac57ed11
Huw <72554909+stevenage-luton@users.noreply.github.com>
2023-01-16 00:49:43 +0000

    Final Build

--- Assets/Scripts/Interactables/equipmentcontainer.cs
+++ Assets/Scripts/Interactables/equipmentcontainer.cs
@@ -2,27 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class equipmentcontainer : MonoBehaviour
+public class equipmentcontainer : Interactable
 {
     public float pushforce = 1;
+
+    private Rigidbody _rigidbody;
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
 
-        Rigidbody _rigidbody = hit.collider.attachedRigidbody;
+        _rigidbody = hit.collider.attachedRigidbody;
 
-        if (Input.GetKeyDown(KeyCode.F))
+
+       
+        if (_rigidbody != null)
         {
-            if (_rigidbody != null)
-            {
-                Vector3 forcedirection = hit.gameObject.transform.position - transform.position;
-                forcedirection.y = 0;
-                forcedirection.Normalize();
+            Vector3 forcedirection = hit.gameObject.transform.position - transform.position;
+            forcedirection.y = 0;
+            forcedirection.Normalize();
 
-                _rigidbody.AddForceAtPosition(forcedirection * pushforce, transform.position, ForceMode.Impulse);
-            }
+            _rigidbody.AddForceAtPosition(forcedirection * pushforce, transform.position, ForceMode.Impulse);
         }
 
     }
+
 
 }
